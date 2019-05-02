4ee2798465b9d08c7026e02a11ae3cdd9cc802db
divinepromise <divinepromise@dal.ca>
2019-05-01 20:40:38 -0700

    second save of task4, dealing with function object as a parameter with a value and printing twice

--- task4.py
+++ task4.py
@@ -1,10 +1,10 @@
-def do_twice(f):
-	f()
-	f()
+def do_twice(f,value):
+	f(value)
+	f(value)
 
 
-def print_spam():
-	print("spam")
+def print_spam(value):
+	print(value)
 
 
-do_twice(print_spam)
+do_twice(print_spam,"spam")
