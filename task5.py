363f674034c5243fbe5ede82ead64c2442468aa0
divinepromise <divinepromise@dal.ca>
2019-05-01 21:04:14 -0700

    final save of task5, drawing of gridlines

--- task5.py
+++ task5.py
@@ -3,16 +3,28 @@ def grid_line():
 	while i<=2:
 		print('+','---',sep='',end="")
 		i+=1
-	
+	print("+")
+
+
 def draw_bar():
 	j=1
 	while j<=2:
 		print('|   ',sep='',end="")
 		j+=1
+	print("|")
+
+
+def draw_bar_loop():
+	i=1
+	while i<=4:
+		draw_bar()
+		i+=1
+
 
 
 
 grid_line()
-print("+")
-draw_bar()
-print("|")
+draw_bar_loop()
+grid_line()
+draw_bar_loop()
+grid_line()
